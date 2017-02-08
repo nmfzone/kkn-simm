73af8a044e31ec52529faa4366bc3a4b82019861
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2017-02-08 10:49:58 +0700

    Update

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -32,6 +32,7 @@ require('vue-resource');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common = {
+  'X-CSRF-TOKEN': window.Laravel.csrfToken,
   'X-Requested-With': 'XMLHttpRequest'
 };
 
